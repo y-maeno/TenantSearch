b885a8e50f07643bfba1db1d6d844e61655e0adb
前野淑美 <maenoyoshimi@MacBook-Air.local>
2018-06-13 17:21:29 +0900

    ブランチの作成とプッシュ

--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,7 +1,7 @@
 class Realestate
   include ActiveModel::Model
 
-  attr_accessor :min_size_lt, :max_size_gt, :floor_eq, :restaurant_eq
+  attr_accessor :min_size_lteq, :max_size_gteq, :floor_eq, :restaurant_eq
 
   # validates :area, presence: true
   # validates :floor, presence: true
